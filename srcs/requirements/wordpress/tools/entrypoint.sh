d4f6d273b60eafe07cc5bc99265bbc64fb9be794
lyricall <lachariotte30@gmail.com>
2025-05-15 09:38:31 +0200

    site ok

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,42 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
-WP_DIR="/var/www/wordpress"
 
-echo "Attente de la connexion à MariaDB..."
-MAX_TRIES=30
-COUNTER=0
-until mysqladmin ping -h mariadb -u ${MYSQL_USER} -p${MYSQL_PASSWORD} --silent; do
-    COUNTER=$((COUNTER+1))
-    if [ $COUNTER -ge $MAX_TRIES ]; then
-        echo "Impossible de se connecter à MariaDB après $MAX_TRIES tentatives. Sortie."
-        exit 1
-    fi
-    echo "En attente de MariaDB (tentative $COUNTER/$MAX_TRIES)..."
-    sleep 5
-done
-echo "MariaDB est prêt!"
+WP_PATH="/var/www/wordpress"
+export HTTP_HOST="$DOMAIN_NAME"
 
-echo "Vérification de l'existence de la base de données ${WORDPRESS_DB_NAME}..."
-if ! mysql -h mariadb -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "USE ${WORDPRESS_DB_NAME}"; then
-    echo "La base de données ${WORDPRESS_DB_NAME} n'existe pas ou n'est pas accessible!"
-    exit 1
-fi
-echo "Base de données ${WORDPRESS_DB_NAME} accessible."
+sleep 10
 
-if [ ! -f "$WP_DIR/wp-config.php" ]; then
-    echo "Téléchargement de WordPress..."
-    wget https://wordpress.org/latest.tar.gz
-    tar -xzf latest.tar.gz
-    cp -a wordpress/. $WP_DIR/
-    rm -rf wordpress latest.tar.gz
-    ./wp-config-create.sh
+if [ ! -f "$WP_PATH/wp-load.php" ]; then
+  echo ">> Téléchargement de WordPress..."
+  wp core download --path="$WP_PATH" --allow-root
 fi
 
-find /var/www/wordpress -type d -exec chmod 755 {} \;
-find /var/www/wordpress -type f -exec chmod 644 {} \;
+if [ ! -f "$WP_PATH/wp-config.php" ]; then
+  echo ">> Création du fichier wp-config.php..."
+  /wp-config-create.sh
+fi
+
+if ! wp core is-installed --path="$WP_PATH" --allow-root; then
+  echo ">> Installation de WordPress..."
+  wp core install --path="$WP_PATH" --allow-root --url=https://$DOMAIN_NAME --title="WordPress Site" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL
+  echo ">> Création des utilisateurs..."
+  ./wp-user.sh
+fi
+
+wp plugin is-active redis-cache --path="$WP_PATH" --allow-root || wp plugin activate redis-cache --path="$WP_PATH" --allow-root
+
+echo ">> Configuration des permissions..."
 chown -R www:www /var/www/wordpress
 
-./wp-user.sh
-echo "Démarrage de PHP-FPM..."
 exec php-fpm81 -F
